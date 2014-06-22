3eba660b61be89d2cf51bcd63c3f2f9c3704d5fd
nik7007 <niky006@gmail.com>
2014-06-22 18:59:53 +0200

    Set the icon of the appropriate block to the creative tab

--- src/main/java/com/nik7/upgcraft/CreativeTab/CreativeTab.java
+++ src/main/java/com/nik7/upgcraft/CreativeTab/CreativeTab.java
@@ -1,16 +1,24 @@
 package com.nik7.upgcraft.CreativeTab;
 
+import com.nik7.upgcraft.block.BlockWoodenLiquidTank;
+import com.nik7.upgcraft.block.InitBlocks;
 import com.nik7.upgcraft.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class CreativeTab {
 
-    public static final CreativeTabs UPGC_TAB = new CreativeTabs(Reference.MOD_ID) {
+    public static final CreativeTabs UPGC_TAB = new CreativeTabs(Reference.MOD_NAME) {
+
         @Override
+        @SideOnly(Side.CLIENT)
         public Item getTabIconItem() {
-            return new Item();
+            return Item.getItemFromBlock(InitBlocks.blockWoodenLiquidTank);
         }
+
+
     };
 
 }
