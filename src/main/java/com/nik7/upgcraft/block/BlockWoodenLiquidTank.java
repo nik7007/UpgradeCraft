e2e9b3f0bba7011f6eaacdf3092f6c6c7df36a10
nik7007 <niky006@gmail.com>
2014-06-24 22:04:44 +0200

    Create tank Entity

--- src/main/java/com/nik7/upgcraft/block/BlockWoodenLiquidTank.java
+++ src/main/java/com/nik7/upgcraft/block/BlockWoodenLiquidTank.java
@@ -3,19 +3,23 @@ package com.nik7.upgcraft.block;
 import com.nik7.upgcraft.reference.Names;
 import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidTank;
 
-public class BlockWoodenLiquidTank extends BlockUpgC {
+public class BlockWoodenLiquidTank extends BlockUpgC implements IFluidTank {
 
     public BlockWoodenLiquidTank() {
         super(Material.wood);
         this.setBlockName(Names.Blocks.WOODENLIQUIDTANK);
         this.setHardness(2.5f);
         this.setBlockBounds(0.0625f, 0.0f, 0.0625f, 0.9375f, 0.875f, 0.9375f); //chest block bounds
+        this.setStepSound(soundTypeWood);
     }
 
     @Override
     public boolean renderAsNormalBlock() {
-        return true;
+        return false;
     }
 
     @Override
@@ -31,7 +35,39 @@ public class BlockWoodenLiquidTank extends BlockUpgC {
         if (world.getBlock(x, y + 1, z) == this)
             if (world.getBlock(x, y + 2, z) == this)
                 return false;
+        if (world.getBlock(x, y + 1, z) == this && world.getBlock(x, y - 1, z) == this)
+            return false;
 
         return true;
+    }
+
+    @Override
+    public FluidStack getFluid() {
+        return null;
+    }
+
+    @Override
+    public int getFluidAmount() {
+        return 0;
+    }
+
+    @Override
+    public int getCapacity() {
+        return 0;
+    }
+
+    @Override
+    public FluidTankInfo getInfo() {
+        return null;
+    }
+
+    @Override
+    public int fill(FluidStack resource, boolean doFill) {
+        return 0;
+    }
+
+    @Override
+    public FluidStack drain(int maxDrain, boolean doDrain) {
+        return null;
     }
 }
