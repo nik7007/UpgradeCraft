0749bdf4f64b9052077c7d2d8ab7f00f64130db3
nik7007 <niky006@gmail.com>
2014-06-28 12:58:10 +0200

    Removed some unnecessary comments

--- src/main/java/com/nik7/upgcraft/block/InitBlocks.java
+++ src/main/java/com/nik7/upgcraft/block/InitBlocks.java
@@ -6,12 +6,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class InitBlocks {
 
     public static final BlockUpgC blockWoodenLiquidTank = new BlockWoodenLiquidTank();
-    //public static final BlockUpgC blockLiquidTank = new BlockUpgCTank();
 
     public static void init() {
 
         GameRegistry.registerBlock(blockWoodenLiquidTank, "blockWoodenLiquidTank");
-        //GameRegistry.registerBlock(blockLiquidTank,"blockLiquidTank");
 
     }
 }
