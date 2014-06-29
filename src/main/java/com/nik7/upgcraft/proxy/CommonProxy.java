92cee7a785be69cc81cb074d616607e29e4de818
nik7007 <niky006@gmail.com>
2014-06-29 09:47:30 +0200

    correct references

--- src/main/java/com/nik7/upgcraft/proxy/CommonProxy.java
+++ src/main/java/com/nik7/upgcraft/proxy/CommonProxy.java
@@ -1,14 +1,13 @@
 package com.nik7.upgcraft.proxy;
 
 import com.nik7.upgcraft.reference.Names;
-import com.nik7.upgcraft.reference.Reference;
-import com.nik7.upgcraft.tileentities.UpgCTileEntityTank;
+import com.nik7.upgcraft.tileentities.UpgCtileentityTank;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public abstract class CommonProxy implements IProxy {
 
     public void registerTileEntities() {
 
-        GameRegistry.registerTileEntity(UpgCTileEntityTank.class, Names.TileEntity.UPGC_TILE_ENTITY_TANK);
+        GameRegistry.registerTileEntity(UpgCtileentityTank.class, Names.TileEntity.UPGC_TILE_ENTITY_TANK);
     }
 }
