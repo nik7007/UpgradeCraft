3bcd585dae2ac381a9112aa6cad0f92c8578cb50
nik7007 <niky006@gmail.com>
2014-07-03 14:39:56 +0200

    Now integration with redston works!!

--- src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityTank.java
+++ src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityTank.java
@@ -1,6 +1,9 @@
 package com.nik7.upgcraft.tileentities;
 
 import com.nik7.upgcraft.block.UpgCTank;
+import net.minecraft.block.Block;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.TileFluidHandler;
 
@@ -13,6 +16,32 @@ public class UpgCtileentityTank extends TileFluidHandler {
 
     public void setTank(FluidTank tank) {
         this.tank = tank;
+    }
+
+    private void updateModBlock() {
+        Block block = this.worldObj.getBlock(xCoord, yCoord, zCoord);
+        this.worldObj.notifyBlockChange(xCoord, yCoord, zCoord, block);
+    }
+
+    @Override
+    public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
+        int result = super.fill(from, resource, doFill);
+        updateModBlock();
+        return result;
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection from, FluidStack resource, boolean doDrain) {
+        FluidStack result = super.drain(from, resource, doDrain);
+        updateModBlock();
+        return result;
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
+        FluidStack result = super.drain(from, maxDrain, doDrain);
+        updateModBlock();
+        return result;
     }
 
 
