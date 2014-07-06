6f7570fe3f440cca68755bcdd7203c4c2e917e8f
nik7007 <niky006@gmail.com>
2014-07-06 14:49:46 +0200

    Add String Helper

---
+++ src/main/java/com/nik7/upgcraft/util/StringHelper.java
@@ -0,0 +1,8 @@
+package com.nik7.upgcraft.util;
+
+public class StringHelper {
+
+    public static String getUnwrappedUnlocalizedName(String unlocalizedName) {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
