7f59b6840cf1ca3f72abaf9553f12c283b63aa70
nik7007 <niky006@gmail.com>
2014-07-06 14:51:56 +0200

    Introduce useful method

--- src/main/java/com/nik7/upgcraft/block/BlockUpgC.java
+++ src/main/java/com/nik7/upgcraft/block/BlockUpgC.java
@@ -1,8 +1,11 @@
 package com.nik7.upgcraft.block;
 
 import com.nik7.upgcraft.CreativeTab.CreativeTab;
+import com.nik7.upgcraft.reference.Names;
+import com.nik7.upgcraft.util.StringHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
 
 public class BlockUpgC extends Block {
 
@@ -13,7 +16,15 @@ public class BlockUpgC extends Block {
     public BlockUpgC(Material material) {
         super(material);
         this.setCreativeTab(CreativeTab.UPGC_TAB);
+
     }
+
+    @Override
+    public String getUnlocalizedName() {
+        return String.format("item.%s%s", Names.RESOURCE_PREFIX, StringHelper.getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+
 
 
 }
