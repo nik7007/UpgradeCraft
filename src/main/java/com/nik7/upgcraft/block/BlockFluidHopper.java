3f9a855d5745d35bdae6b51799bd732aed5d6a27
nik7 <nik7007@users.noreply.github.com>
2015-02-04 14:22:17 +0100

    -Rename fluid hopper in basic fluid hopper.  - Basic functionality works properly

--- src/main/java/com/nik7/upgcraft/block/BlockFluidHopper.java
+++ src/main/java/com/nik7/upgcraft/block/BlockFluidHopper.java
@@ -3,6 +3,7 @@ package com.nik7.upgcraft.block;
 
 import com.nik7.upgcraft.reference.Names;
 import com.nik7.upgcraft.tileentities.UpgCraftFluidHopper;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Facing;
@@ -13,19 +14,48 @@ public class BlockFluidHopper extends BlockUpgCTank {
 
     public BlockFluidHopper() {
         super(Material.iron);
-        setBlockName(Names.Blocks.FLUID_HOPPER);
+        setBlockName("Basic" + Names.Blocks.FLUID_HOPPER);
     }
 
-    public int onBlockPlaced(World world, int x, int y, int z, int hitX, int hitY, int hitZ, int meta)
-    {
-        int newMeta = Facing.oppositeSide[hitX];
+    public static int getDirectionFromMetadata(int meta) {
+        return meta & 7;
+    }
 
-        if(newMeta==1)
-        {
+    public static boolean isNotPowered(int meta) {
+        return (meta & 8) != 8;
+    }
+
+    @Override
+    public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int meta) {
+        int newMeta = Facing.oppositeSide[side];
+
+        if (newMeta == 1) {
             newMeta = 0;
         }
 
         return newMeta;
+    }
+
+    @Override
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+
+        boolean isNotGettingPower = !world.isBlockIndirectlyGettingPowered(x, y, z);
+        int meta = world.getBlockMetadata(x, y, z);
+        int dir = getDirectionFromMetadata(meta);
+        boolean flag = isNotPowered(meta);
+        if (isNotGettingPower != flag) {
+            world.setBlockMetadataWithNotify(x, y, z, dir | (isNotGettingPower ? 0 : 8), 4);
+        }
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
     }
 
     @Override
--- src/main/java/com/nik7/upgcraft/tileentities/UpgCraftFluidHopper.java
+++ src/main/java/com/nik7/upgcraft/tileentities/UpgCraftFluidHopper.java
@@ -1,8 +1,10 @@
 package com.nik7.upgcraft.tileentities;
 
 
+import com.nik7.upgcraft.block.BlockFluidHopper;
 import com.nik7.upgcraft.inventory.UpgCTank;
 import com.nik7.upgcraft.reference.Capacity;
+import com.nik7.upgcraft.util.LogHelper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
@@ -20,30 +22,56 @@ public class UpgCraftFluidHopper extends UpgCtileentityTank {
 
     @Override
     public void updateEntity() {
-        fillFromUp();
+        int meta = this.getBlockMetadata();
+        if (worldObj != null && !this.worldObj.isRemote && BlockFluidHopper.isNotPowered(meta)) {
+            fillFromUp();
+            int dir = BlockFluidHopper.getDirectionFromMetadata(meta);
+            ForgeDirection forgeDirection = ForgeDirection.getOrientation(dir);
+            autoDrain(forgeDirection);
+        }
 
     }
 
     private void fillFromUp() {
-        if (worldObj != null) {
-            if (worldObj.getTileEntity(xCoord, yCoord + 1, zCoord) instanceof IFluidHandler) {
-                IFluidHandler fluidHandler = (IFluidHandler) worldObj.getTileEntity(xCoord, yCoord + 1, zCoord);
-                FluidStack fluidStack;
-                if ((fluidStack = fluidHandler.drain(ForgeDirection.DOWN, 1, false)) != null) {
 
-                    fluidStack = new FluidStack(fluidStack, speed);
+        if (worldObj.getTileEntity(xCoord, yCoord + 1, zCoord) instanceof IFluidHandler) {
+            IFluidHandler fluidHandler = (IFluidHandler) worldObj.getTileEntity(xCoord, yCoord + 1, zCoord);
+            FluidStack fluidStack;
+            if ((fluidStack = fluidHandler.drain(ForgeDirection.DOWN, 1, false)) != null) {
 
-                    int cacity = getTank().fill(fluidStack, false);
-                    if (cacity > 0) {
-                        fluidStack = fluidHandler.drain(ForgeDirection.DOWN, cacity, true);
-                        super.fill(ForgeDirection.UP, fluidStack, true);
+                fluidStack = new FluidStack(fluidStack, speed);
 
+                int cacity = getTank().fill(fluidStack, false);
+                if (cacity > 0) {
+                    fluidStack = fluidHandler.drain(ForgeDirection.DOWN, cacity, true);
+                    super.fill(ForgeDirection.UP, fluidStack, true);
+
+                }
+            }
+
+
+        }
+
+    }
+
+    private void autoDrain(ForgeDirection direction) {
+
+        if (!this.isEmpty()) {
+            if (worldObj.getTileEntity(xCoord + direction.offsetX, yCoord + direction.offsetY, zCoord + direction.offsetZ) instanceof IFluidHandler) {
+                IFluidHandler fluidHandler = (IFluidHandler) worldObj.getTileEntity(xCoord + direction.offsetX, yCoord + direction.offsetY, zCoord + direction.offsetZ);
+
+                FluidStack fluidStack = new FluidStack(this.getFluid(), speed);
+                int amount;
+                if ((amount = fluidHandler.fill(direction.getOpposite(), fluidStack, false)) > 0) {
+                    fluidStack = this.drain(direction, amount, true);
+                    if (amount != fluidHandler.fill(direction.getOpposite(), fluidStack, true)) {
+                        LogHelper.error("Something wrong appends: Fluid is transferring wrong!!");
                     }
                 }
 
-
             }
         }
+
     }
 
 
