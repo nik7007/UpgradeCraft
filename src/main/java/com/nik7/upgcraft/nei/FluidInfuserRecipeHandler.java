99f870618792936ca91ed2ab719bedbc56423354
nik7 <niky006@gmail.com>
2015-08-05 12:34:45 +0200

    Add fluid tank support for NEI

--- src/main/java/com/nik7/upgcraft/nei/FluidInfuserRecipeHandler.java
+++ src/main/java/com/nik7/upgcraft/nei/FluidInfuserRecipeHandler.java
@@ -13,14 +13,8 @@ import com.nik7.upgcraft.reference.Texture;
 import com.nik7.upgcraft.registry.FluidInfuser.FluidInfuserRecipe;
 import com.nik7.upgcraft.registry.FluidInfuserRegister;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.IIcon;
 import net.minecraft.util.StatCollector;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
-import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -35,14 +29,16 @@ public class FluidInfuserRecipeHandler extends TemplateRecipeHandler {
 
         public PositionedStack output;
 
-        public FluidStack fluidStack;
+        //public FluidStack fluidStack;
+        public PositionedFluidTank positionedFluidTank;
 
         public CachedFluidInfuserRecipe(FluidInfuserRecipe recipe) {
 
             ItemStack toMelt = recipe.getInputs().getToMelt();
             ItemStack toInfuse = recipe.getInputs().getToInfuse();
             ItemStack result = recipe.getResult();
-            fluidStack = recipe.getFluidStack();
+            //fluidStack = recipe.getFluidStack();
+            positionedFluidTank = new PositionedFluidTank(recipe.getFluidStack(), Capacity.INTERNAL_FLUID_TANK_TR1, 11, 45, 16, 32);
 
             inputs = Arrays.asList(new PositionedStack(toMelt, 41, 6), new PositionedStack(toInfuse, 71, 6));
             output = new PositionedStack(result, 131, 24);
@@ -70,52 +66,22 @@ public class FluidInfuserRecipeHandler extends TemplateRecipeHandler {
     @Override
     public void drawExtras(int recipe) {
 
-        FluidStack fluidStack = ((CachedFluidInfuserRecipe) arecipes.get(recipe)).fluidStack;
+        PositionedFluidTank fluidStack = ((CachedFluidInfuserRecipe) arecipes.get(recipe)).positionedFluidTank;
 
         if (fluidStack != null) {
-            Fluid fluid = fluidStack.getFluid();
-            if (fluid != null) {
-                int level = Math.round(((float) fluidStack.amount / (float) Capacity.INTERNAL_FLUID_TANK_TR1) * 100);
-
-                if (level == 0 && fluidStack.amount != 0)
-                    level = 1;
-                if (level > 32)
-                    level = 32;
-
-                IIcon icon = fluid.getIcon();
-
-                int color = fluid.getColor();
-
-                GuiDraw.changeTexture(TextureMap.locationBlocksTexture);
-
-                GL11.glColor3ub((byte) (color >> 16 & 0xFF), (byte) (color >> 8 & 0xFF), (byte) (color & 0xFF));
-                GL11.glDisable(GL11.GL_BLEND);
-
-                GuiDraw.gui.drawTexturedModelRectFromIcon(11, 45 - level, icon, 16, level);
-
-                GL11.glEnable(GL11.GL_BLEND);
-            }
+            fluidStack.draw();
         }
         GuiDraw.changeTexture(getGuiTexture());
         this.drawProgressBar(41, 25, 176, 0, 15, 15, 20, 3);
-        this.drawProgressBar(94, 23, 176, 14,25, 16, 60, 0);
+        this.drawProgressBar(94, 23, 176, 14, 25, 16, 60, 0);
     }
 
     @Override
     public List<String> handleTooltip(GuiRecipe gui, List<String> currenttip, int recipe) {
         currenttip = super.handleTooltip(gui, currenttip, recipe);
-        FluidStack fluidStack = ((CachedFluidInfuserRecipe) arecipes.get(recipe)).fluidStack;
-        Point offset = gui.getRecipePosition(recipe);
-        Point mousepos = GuiDraw.getMousePosition();
-        Point relMouse = new Point(mousepos.x - (gui.width - 176) / 2 - offset.x, mousepos.y - (gui.height - 164) / 2 - offset.y);
 
-        if ((relMouse.x >= 11 && relMouse.x <= 11 + 16) && (relMouse.y < 45 && relMouse.y > 45 - 35)) {
-            currenttip.add(EnumChatFormatting.AQUA + StatCollector.translateToLocal("tooltip." + Reference.MOD_ID + ":tank.fluidname") + ": " + EnumChatFormatting.RESET + fluidStack.getLocalizedName());
-            currenttip.add(EnumChatFormatting.AQUA + StatCollector.translateToLocal("tooltip." + Reference.MOD_ID + ":tank.fluidamount") + ": " + EnumChatFormatting.RESET + fluidStack.amount + " mB");
-        }
-
-
-        return currenttip;
+        PositionedFluidTank fluidStack = ((CachedFluidInfuserRecipe) arecipes.get(recipe)).positionedFluidTank;
+        return fluidStack.handleTooltip(gui, currenttip, recipe);
     }
 
 
@@ -167,7 +133,7 @@ public class FluidInfuserRecipeHandler extends TemplateRecipeHandler {
 
     @Override
     public void loadTransferRects() {
-        transferRects.add(new RecipeTransferRect(new Rectangle(67, 23, 50,16), getRecipeID()));
+        transferRects.add(new RecipeTransferRect(new Rectangle(67, 23, 50, 16), getRecipeID()));
     }
 
 
