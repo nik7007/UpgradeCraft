bf599afac026991f7bae494ba19b287eb9519b9b
nik7 <niky006@gmail.com>
2015-08-18 17:03:36 +0200

    This should exist only at client side

--- src/main/java/com/nik7/upgcraft/client/render/item/itemRenderTermoFluidFurnace.java
+++ src/main/java/com/nik7/upgcraft/client/render/item/itemRenderTermoFluidFurnace.java
@@ -3,9 +3,12 @@ package com.nik7.upgcraft.client.render.item;
 
 import com.nik7.upgcraft.reference.Texture;
 import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
+@SideOnly(Side.CLIENT)
 public class itemRenderTermoFluidFurnace extends ItemRenderMachine {
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
