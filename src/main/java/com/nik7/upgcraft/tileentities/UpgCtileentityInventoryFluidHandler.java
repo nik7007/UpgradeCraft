c022a656d1b8aacf2a6a7d591991e51fcacd697c
nik7 <niky006@gmail.com>
2015-08-20 14:27:57 +0200

    Sync bug fix

--- src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityInventoryFluidHandler.java
+++ src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityInventoryFluidHandler.java
@@ -93,7 +93,7 @@ public abstract class UpgCtileentityInventoryFluidHandler extends TileEntity imp
         int fluidID = buf.readInt();
         if (fluidAmount > 0) {
             tank.setFluid(new FluidStack(FluidRegistry.getFluid(fluidID), fluidAmount));
-        }
+        } else tank.setFluid(null);
 
         worldObj.markBlockRangeForRenderUpdate(xCoord, yCoord, zCoord, xCoord, yCoord, zCoord);
 
