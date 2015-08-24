59a03299feab3d192791553f7715ac38f3e1b76e
Nik <niky006@gmail.com>
2015-08-24 19:02:05 +0200

    Bug fix

--- src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityInventoryFluidHandler.java
+++ src/main/java/com/nik7/upgcraft/tileentities/UpgCtileentityInventoryFluidHandler.java
@@ -75,7 +75,7 @@ public abstract class UpgCtileentityInventoryFluidHandler extends TileEntity imp
 
     protected void writeFluidToByteBuf(FluidTank tank, ByteBuf buf) {
 
-        FluidStack fluidStack = tank != null ? this.tank[0].getFluid() : null;
+        FluidStack fluidStack = tank != null ? tank.getFluid() : null;
         int fluidAmount = -1;
         int fluidID = -1;
         if (fluidStack != null) {
