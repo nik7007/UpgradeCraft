659426264f7a6549ad01ffb5189518590ab9f9a0
nik7 <nik7007@users.noreply.github.com>
2015-09-21 17:32:26 +0200

    In this way properties look nicer...

--- src/main/java/com/nik7/upgcraft/handler/ConfigurationHandler.java
+++ src/main/java/com/nik7/upgcraft/handler/ConfigurationHandler.java
@@ -24,9 +24,9 @@ public class ConfigurationHandler {
 
     private static void loadConfiguration() {
 
-        SystemConfig.getInstance().basicWoodenBlockFlammability = configuration.getBoolean("basic_Wooden_Block_Flammability", Configuration.CATEGORY_GENERAL, true, "Set to false to make fireproof Wooden Liquid Tank, and Slimy Log.");
+        SystemConfig.getInstance().basicWoodenBlockFlammability = configuration.getBoolean("basic Wooden Block Flammability", Configuration.CATEGORY_GENERAL, true, "Set to false to make fireproof Wooden Liquid Tank, and Slimy Log.");
 
-        SystemConfig.getInstance().basicTankCapacity = configuration.getInt("basic_tank_capacity", Configuration.CATEGORY_GENERAL, 27, 24, 32, "Basic capacity Tank custom value. This will not change the capacity of the enderTanks.");
+        SystemConfig.getInstance().basicTankCapacity = configuration.getInt("basic tank capacity", Configuration.CATEGORY_GENERAL, 27, 24, 32, "Basic capacity Tank custom value. This will not change the capacity of the enderTanks.");
 
         if (configuration.hasChanged()) {
             configuration.save();
