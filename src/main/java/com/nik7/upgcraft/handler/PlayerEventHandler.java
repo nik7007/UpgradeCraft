81d4d480f2002996e3c59e0ef3d4f3fb438c5e27
nik7 <nik7007@users.noreply.github.com>
2016-03-21 19:39:49 +0100

    Add player events for the ender fluid tank

--- src/main/java/com/nik7/upgcraft/handler/PlayerEventHandler.java
+++ src/main/java/com/nik7/upgcraft/handler/PlayerEventHandler.java
@@ -1,24 +1,49 @@
 package com.nik7.upgcraft.handler;
 
 
-import com.nik7.upgcraft.entity.player.ExtendedPlayerUpgC;
+import com.nik7.upgcraft.reference.Reference;
+import com.nik7.upgcraft.tank.UpgCEnderFluidTank;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static com.nik7.upgcraft.handler.CapabilityPlayerUpgCHandler.Provider.PLAYER_UPGC;
 
 public class PlayerEventHandler {
 
     @SubscribeEvent
-    public void onEntityConstructing(EntityEvent.EntityConstructing event) {/*
-        if (event.entity instanceof EntityPlayer && ExtendedPlayerUpgC.get((EntityPlayer) event.entity) == null) {
-            //ExtendedPlayerUpgC.register((EntityPlayer) event.entity);
-        }*/
+    public void onEntityConstruct(AttachCapabilitiesEvent.Entity entity) {
+
+        if (entity.getEntity() instanceof EntityPlayer) {
+            entity.addCapability(new ResourceLocation(Reference.MOD_ID + "playerCapabilities"), new CapabilityPlayerUpgCHandler.Provider());
+
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerDeath(PlayerEvent.Clone e) {
+        if (e.wasDeath) {
+            EntityPlayer playerOriginal = e.original;
+            EntityPlayer playerNew = e.entityPlayer;
+
+            if (playerOriginal.hasCapability(PLAYER_UPGC, null) && playerNew.hasCapability(PLAYER_UPGC, null)) {
+
+                FluidStack fluidStack = playerOriginal.getCapability(PLAYER_UPGC, null).getEnderFluidTank().getFluid();
+                UpgCEnderFluidTank fluidTank = playerNew.getCapability(PLAYER_UPGC, null).getEnderFluidTank();
+                fluidTank.fill(fluidStack, true);
+            }
+
+        }
+
     }
 
     public static void init() {
 
-        //MinecraftForge.EVENT_BUS.register(new PlayerEventHandler());
+        MinecraftForge.EVENT_BUS.register(new PlayerEventHandler());
     }
 
 }
