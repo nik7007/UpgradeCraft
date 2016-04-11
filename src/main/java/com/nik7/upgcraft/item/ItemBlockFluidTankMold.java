31806ca35041cca8199cb1633d57345a07485654
nik7 <nik7007@users.noreply.github.com>
2016-04-12 00:59:40 +0200

    Get XP when crafted (or smelted)

--- src/main/java/com/nik7/upgcraft/item/ItemBlockFluidTankMold.java
+++ src/main/java/com/nik7/upgcraft/item/ItemBlockFluidTankMold.java
@@ -3,6 +3,7 @@ package com.nik7.upgcraft.item;
 
 import com.nik7.upgcraft.init.ModBlocks;
 import com.nik7.upgcraft.reference.Reference;
+import com.nik7.upgcraft.registry.CustomCraftingExperience;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -15,7 +16,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ItemBlockFluidTankMold extends ItemBlock {
+public class ItemBlockFluidTankMold extends ItemBlock implements CustomCraftingExperience {
 
     public ItemBlockFluidTankMold(Block block) {
         super(block);
@@ -48,5 +49,12 @@ public class ItemBlockFluidTankMold extends ItemBlock {
     @Override
     public int getMetadata(int damage) {
         return damage;
+    }
+
+    @Override
+    public float getCustomCraftingExperience(ItemStack item) {
+
+        float value = item.getItemDamage() / 5f;
+        return 0.65f * value + 0.35f;
     }
 }
