e06b4c677e8be881aebfe1e21df6e8bb26d6708d
nik7 <nik7007@users.noreply.github.com>
2016-04-18 15:00:32 +0200

    Safety way to register message

--- src/main/java/com/nik7/upgcraft/network/NetworkHandler.java
+++ src/main/java/com/nik7/upgcraft/network/NetworkHandler.java
@@ -3,17 +3,24 @@ package com.nik7.upgcraft.network;
 
 import com.nik7.upgcraft.reference.Reference;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
 
 public class NetworkHandler {
     private static SimpleNetworkWrapper INSTANCE;
+    private static int ID = 0;
 
     public static void init() {
         INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(Reference.MOD_ID);
-        INSTANCE.registerMessage(new MessageUpdateRequestHandler(), UpdateRequestMessage.class, 0, Side.SERVER);
-        INSTANCE.registerMessage(new MessageHardenedFluidTankHandler(), UpdateRequestMessage.class, 1, Side.CLIENT);
+        NetworkHandler.registerMessage(new MessageUpdateRequestHandler(), UpdateRequestMessage.class, Side.SERVER);
+        NetworkHandler.registerMessage(new MessageHardenedFluidTankHandler(), UpdateRequestMessage.class, Side.CLIENT);
+    }
+
+    private static <REQ extends IMessage, REPLY extends IMessage> void registerMessage(IMessageHandler<? super REQ, ? extends REPLY> messageHandler, Class<REQ> requestMessageType, Side side) {
+        INSTANCE.registerMessage(messageHandler, requestMessageType, ID++, side);
     }
 
     public static SimpleNetworkWrapper getInstance() {
