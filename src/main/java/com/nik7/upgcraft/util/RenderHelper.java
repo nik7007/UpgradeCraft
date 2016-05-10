d43fd5b09b931dfdc7fffbe4c999f3801788eaa0
nik7 <nik7007@users.noreply.github.com>
2016-05-10 11:42:04 +0200

    Removed unnecessary stuff

--- src/main/java/com/nik7/upgcraft/util/RenderHelper.java
+++ src/main/java/com/nik7/upgcraft/util/RenderHelper.java
@@ -227,10 +227,6 @@ public class RenderHelper {
                 f11 = f12 = f13 = 1.0F * f6;
             }
 
-            /*worldrenderer.pos(x, y + (double)f3, z).color(f11, f12, f13, 1.0F).endVertex();
-            worldrenderer.pos(x, y + (double)f3, z + 1.0D).color(f11, f12, f13, 1.0F).endVertex();
-            worldrenderer.pos(x + 1.0D, y + (double)f3, z + 1.0D).color(f11, f12, f13, 1.0F).endVertex();
-            worldrenderer.pos(x + 1.0D, y + (double)f3, z).color(f11, f12, f13, 1.0F).endVertex();*/
 
             //north
             addVertexWithColor(RenderHelper.vertexBuffer, xMax, yMin, zMin, f11, f12, f13, 1.0F);
