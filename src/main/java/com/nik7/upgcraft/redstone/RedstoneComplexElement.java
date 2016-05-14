b26c059d5a9ce1f8ed55932135c39739af0363e6
Nik <niky006@gmail.com>
2016-05-14 15:01:11 +0200

    More working on redstone logic

---
+++ src/main/java/com/nik7/upgcraft/redstone/RedstoneComplexElement.java
@@ -0,0 +1,15 @@
+package com.nik7.upgcraft.redstone;
+
+
+public class RedstoneComplexElement extends RedstoneElement {
+
+
+    public RedstoneComplexElement(Connection[] connections, int tickTocomplete) {
+        super(ExpressionType.CUSTOM, connections, tickTocomplete);
+    }
+
+    @Override
+    protected void myExec() {
+
+    }
+}
