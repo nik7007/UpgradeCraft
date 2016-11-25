c9144a48294e60349a583ca32f8aba111798e142
nik7 <nik7007@users.noreply.github.com>
2016-11-25 13:43:21 +0100

    Add tanks variant

--- src/main/java/com/nik7/upgcraft/block/FluidTank.java
+++ src/main/java/com/nik7/upgcraft/block/FluidTank.java
@@ -3,10 +3,16 @@ package com.nik7.upgcraft.block;
 
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -19,9 +25,11 @@ import javax.annotation.Nullable;
 public class FluidTank extends BlockUpgC /*implements ITileEntityProvider */ {
 
     private static final AxisAlignedBB BB = new AxisAlignedBB(0.0625f, 0.0f, 0.0625f, 0.9375f, 1.0f, 0.9375f);
+    public static final PropertyBool GLASSED = PropertyBool.create("glassed");
 
     public FluidTank() {
         super(Material.WOOD, "fluidtank");
+        this.setDefaultState(this.blockState.getBaseState().withProperty(GLASSED, false));
     }
 
     @Override
@@ -29,7 +37,7 @@ public class FluidTank extends BlockUpgC /*implements ITileEntityProvider */ {
         return BB;
     }
 
-   public boolean isOpaqueCube(IBlockState state) {
+    public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
 
@@ -41,6 +49,34 @@ public class FluidTank extends BlockUpgC /*implements ITileEntityProvider */ {
     @SideOnly(Side.CLIENT)
     public BlockRenderLayer getBlockLayer() {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, GLASSED);
+    }
+
+    @Override
+    public int damageDropped(IBlockState state) {
+        return getMetaFromState(state);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState().withProperty(GLASSED, meta == 1);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+
+        return state.getValue(GLASSED) ? 1 : 0;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, NonNullList<ItemStack> list) {
+        list.add(new ItemStack(this, 1, 0));
+        list.add(new ItemStack(this, 1, 1));
     }
 
     /*@Nullable
