1bd3c5a330306fc790302c23a7874820e5ddfe77
nik7 <nik7007@users.noreply.github.com>
2016-11-25 18:44:38 +0100

    Start working on fluid tank

---
+++ src/main/java/com/nik7/upgcraft/fluids/tank/UpgCFluidTank.java
@@ -0,0 +1,59 @@
+package com.nik7.upgcraft.fluids.tank;
+
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidTankProperties;
+
+import javax.annotation.Nullable;
+
+public class UpgCFluidTank implements IFluidTank, IFluidHandler {
+
+    private FluidStack fluidStack;
+    private int capacity;
+    protected IFluidTankProperties[] tankProperties;
+
+    @Nullable
+    @Override
+    public FluidStack getFluid() {
+        return fluidStack;
+    }
+
+    @Override
+    public int getFluidAmount() {
+        return fluidStack == null ? 0 : fluidStack.amount;
+    }
+
+    @Override
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public FluidTankInfo getInfo() {
+        return new FluidTankInfo(fluidStack, capacity);
+    }
+
+    @Override
+    public IFluidTankProperties[] getTankProperties() {
+        return new IFluidTankProperties[0];
+    }
+
+    @Override
+    public int fill(FluidStack resource, boolean doFill) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public FluidStack drain(FluidStack resource, boolean doDrain) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public FluidStack drain(int maxDrain, boolean doDrain) {
+        return null;
+    }
+}
