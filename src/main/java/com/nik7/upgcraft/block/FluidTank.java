ddb66e54e9a8e3b62dcc0a57f059dc01a4c44f7b
nik7 <nik7007@users.noreply.github.com>
2016-11-28 14:23:26 +0100

    Working on double fluid tank:  Add property to understand how to render the fluid tank

--- src/main/java/com/nik7/upgcraft/block/FluidTank.java
+++ src/main/java/com/nik7/upgcraft/block/FluidTank.java
@@ -2,9 +2,11 @@ package com.nik7.upgcraft.block;
 
 
 import com.nik7.upgcraft.tileentity.TileEntityFluidTank;
+import com.nik7.upgcraft.util.WorldHelper;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -14,10 +16,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemAir;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -35,10 +34,11 @@ public class FluidTank extends BlockUpgC implements ITileEntityProvider {
 
     private static final AxisAlignedBB BB = new AxisAlignedBB(0.0625f, 0.0f, 0.0625f, 0.9375f, 1.0f, 0.9375f);
     public static final PropertyBool GLASSED = PropertyBool.create("glassed");
+    public static final PropertyEnum<RenderInformation> RENDER_INFORMATION = PropertyEnum.create("render_information", RenderInformation.class);
 
     public FluidTank() {
         super(Material.WOOD, "fluidtank");
-        this.setDefaultState(this.blockState.getBaseState().withProperty(GLASSED, false));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(GLASSED, false).withProperty(RENDER_INFORMATION, RenderInformation.SINGLE));
     }
 
     @Override
@@ -52,6 +52,42 @@ public class FluidTank extends BlockUpgC implements ITileEntityProvider {
 
     public boolean isFullCube(IBlockState state) {
         return false;
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+
+        BlockPos up = pos.up();
+        BlockPos down = pos.down();
+
+        return (!(WorldHelper.getBlock(worldIn, up) == this && WorldHelper.getBlock(worldIn, up.up()) == this) && !(WorldHelper.getBlock(worldIn, down) == this && WorldHelper.getBlock(worldIn, down.down()) == this) && !(WorldHelper.getBlock(worldIn, up) == this && WorldHelper.getBlock(worldIn, down) == this)) && worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos);
+
+    }
+
+    private boolean isDouble(IBlockAccess world, BlockPos pos) {
+
+        return world.getBlockState(pos.up()).getBlock() == this || world.getBlockState(pos.down()).getBlock() == this;
+    }
+
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+
+        boolean isDouble = isDouble(world, pos);
+        boolean isTop;
+        boolean equal;
+        boolean isGLASSED = state.getValue(GLASSED);
+        RenderInformation information = RenderInformation.getRenderInformation(isGLASSED);
+
+        if (isDouble) {
+            isTop = world.getBlockState(pos.down()).getBlock() == this;
+            if (isTop) {
+                equal = isGLASSED == world.getBlockState(pos.down()).getValue(GLASSED);
+            } else
+                equal = isGLASSED == world.getBlockState(pos.up()).getValue(GLASSED);
+            information = RenderInformation.getRenderInformation(true, isTop, isGLASSED, equal);
+        }
+
+        return state.withProperty(RENDER_INFORMATION, information);
     }
 
     @Override
@@ -91,7 +127,7 @@ public class FluidTank extends BlockUpgC implements ITileEntityProvider {
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, GLASSED);
+        return new BlockStateContainer(this, GLASSED, RENDER_INFORMATION);
     }
 
     @Override
@@ -158,4 +194,57 @@ public class FluidTank extends BlockUpgC implements ITileEntityProvider {
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileEntityFluidTank();
     }
+
+    public enum RenderInformation implements IStringSerializable {
+        SINGLE("single"),
+        SINGLE_GLASSED("single_glassed"),
+        DOUBLE_UP("double_up"),
+        DOUBLE_DOWN("double_down"),
+        DOUBLE_GASSED_UP("double_glassed_up"),
+        DOUBLE_GASSED_DOWN("double_glassed_down"),
+        DOUBLE_GASSED_UP_P("double_glassed_up_p"),
+        DOUBLE_GASSED_DOWN_P("double_glassed_down_p");
+
+
+        private final String name;
+
+        public static RenderInformation getRenderInformation(boolean isGlassed) {
+            return getRenderInformation(false, false, isGlassed, false);
+        }
+
+        public static RenderInformation getRenderInformation(boolean isDouble, boolean isTop, boolean isGlassed, boolean isEqual) {
+
+            if (isDouble) {
+                if (isTop) {
+                    if (isGlassed) {
+                        if (isEqual) {
+                            return DOUBLE_GASSED_UP;
+                        }
+                        return DOUBLE_GASSED_UP_P;
+                    }
+                    return DOUBLE_UP;
+                } else {
+                    if (isGlassed) {
+                        if (isEqual) {
+                            return DOUBLE_GASSED_DOWN;
+                        }
+                        return DOUBLE_GASSED_DOWN_P;
+                    }
+                    return DOUBLE_DOWN;
+                }
+            } else if (isGlassed)
+                return SINGLE_GLASSED;
+            return SINGLE;
+        }
+
+        RenderInformation(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+    }
+
 }
