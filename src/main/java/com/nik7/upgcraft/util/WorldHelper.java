84fae10c180d4d6b8e44db06888a9fe0b8395533
nik7 <nik7007@users.noreply.github.com>
2016-11-28 14:29:37 +0100

    Make the method more general

--- src/main/java/com/nik7/upgcraft/util/WorldHelper.java
+++ src/main/java/com/nik7/upgcraft/util/WorldHelper.java
@@ -3,11 +3,12 @@ package com.nik7.upgcraft.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class WorldHelper {
 
-    public static Block getBlock(World worldIn, BlockPos pos) {
+    public static Block getBlock(IBlockAccess worldIn, BlockPos pos) {
 
         return worldIn.getBlockState(pos).getBlock();
 
