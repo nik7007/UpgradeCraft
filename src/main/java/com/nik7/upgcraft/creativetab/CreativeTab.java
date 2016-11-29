87eeccfe7da13d820fa7d869d5814cd7a2746240
nik7 <nik7007@users.noreply.github.com>
2016-11-29 22:45:35 +0100

    Now creative tab has the appropriate icon!

--- src/main/java/com/nik7/upgcraft/creativetab/CreativeTab.java
+++ src/main/java/com/nik7/upgcraft/creativetab/CreativeTab.java
@@ -1,10 +1,9 @@
 package com.nik7.upgcraft.creativetab;
 
 
+import com.nik7.upgcraft.init.ModBlocks;
 import com.nik7.upgcraft.reference.Reference;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -16,9 +15,8 @@ public class CreativeTab {
         @Override
         @SideOnly(Side.CLIENT)
         public ItemStack getTabIconItem() {
-            return new ItemStack(Blocks.SLIME_BLOCK);
+            return new ItemStack(ModBlocks.woodenFluidTank, 1, 1);
         }
-
 
     };
 }
