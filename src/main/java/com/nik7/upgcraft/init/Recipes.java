0cfbe77883c9a8968100397987bb17a57807224a
nik7 <nik7007@users.noreply.github.com>
2016-11-29 22:37:53 +0100

    Add recipes

--- src/main/java/com/nik7/upgcraft/init/Recipes.java
+++ src/main/java/com/nik7/upgcraft/init/Recipes.java
@@ -6,9 +6,11 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
 
-    public static void init(){
+    public static void init() {
 
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.slimyLog), "sss", "sps", "sss", 's', "slimeball", 'p', "plankWood"));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.woodenFluidTank, 1, 0), "sss", "s s", "sss", 's', new ItemStack(ModBlocks.slimyLog));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.woodenFluidTank, 1, 1), "sss", "sgs", "sss", 'g', "blockGlass", 's', new ItemStack(ModBlocks.slimyLog)));
 
     }
 
