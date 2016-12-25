972e4a67a22524df0bcc807329aacf3590aa8701
nik7 <nik7007@users.noreply.github.com>
2016-12-25 16:58:06 +0100

    Add property

--- src/main/java/com/nik7/upgcraft/reference/ConfigOptions.java
+++ src/main/java/com/nik7/upgcraft/reference/ConfigOptions.java
@@ -5,22 +5,20 @@ import net.minecraftforge.fluids.Fluid;
 
 public class ConfigOptions {
 
+    public static final int FUNNEL_CAPACITY = 5 * Fluid.BUCKET_VOLUME;
+    public static final int BASIC_FUNNEL_SPEED = 50;
     //CAPACITY
     public static int CONFIG_CAPACITY ;
     public static int BASIC_CAPACITY = CONFIG_CAPACITY * Fluid.BUCKET_VOLUME;
     public static int DOUBLE_CAPACITY = 2 * BASIC_CAPACITY;
-
-    public static final int FUNNEL_CAPACITY = 5 * Fluid.BUCKET_VOLUME;
-
-    public static void reloadOptions(){
-        BASIC_CAPACITY = CONFIG_CAPACITY * Fluid.BUCKET_VOLUME;
-        DOUBLE_CAPACITY = 2 * BASIC_CAPACITY;
-    }
-
-
     //BURN
     public static boolean WOODEN_TANK_BURN;
     public static boolean WOODEN_FLUID_HOPPER_BURN;
+
+    public static void reloadOptions() {
+        BASIC_CAPACITY = CONFIG_CAPACITY * Fluid.BUCKET_VOLUME;
+        DOUBLE_CAPACITY = 2 * BASIC_CAPACITY;
+    }
 
 
 }
