0264786bc1c97ffc0c697ab5b1f62d4b071da2d4
Nik <niky006@gmail.com>
2016-12-26 16:31:54 +0100

    Burn the block when needed

--- src/main/java/com/nik7/upgcraft/tileentity/TileEntityBasicFunnel.java
+++ src/main/java/com/nik7/upgcraft/tileentity/TileEntityBasicFunnel.java
@@ -3,16 +3,62 @@ package com.nik7.upgcraft.tileentity;
 
 import com.nik7.upgcraft.block.BlockBasicFunnel;
 import com.nik7.upgcraft.reference.ConfigOptions;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraftforge.fluids.FluidStack;
+
+import java.util.Random;
+
 
 public class TileEntityBasicFunnel extends TileEntityFunnel {
+
+    private final static Random RANDOM = new Random();
+    private boolean burned = false;
+    private int burnTicks = RANDOM.nextInt(150) + 1;
+    private int realSpeed = speed;
 
     public TileEntityBasicFunnel() {
         super(ConfigOptions.BASIC_FUNNEL_SPEED);
     }
 
     protected boolean canOperate(IBlockState blockState) {
-        return blockState.getValue(BlockBasicFunnel.BURNED) || super.canOperate(blockState);
+
+        if (!this.burned)
+            this.burned = blockState.getValue(BlockBasicFunnel.BURNED);
+        return this.burned || super.canOperate(blockState);
+    }
+
+
+    private boolean isFluidHot() {
+        FluidStack fluidStack = fluidTank.getFluid();
+        return fluidStack != null && fluidStack.getFluid().getTemperature(fluidStack) > TileEntityWoodenFluidTank.WOOD_BURN_TEMPERATURE;
+    }
+
+    private void burnAction() {
+
+        if (!this.burned && isFluidHot()) {
+            if (this.burnTicks <= 0) {
+                IBlockState state = worldObj.getBlockState(pos);
+                Block block = state.getBlock();
+
+                if (block instanceof BlockBasicFunnel)
+                    ((BlockBasicFunnel) block).burnFunnel(worldObj, pos, state, RANDOM);
+
+            } else this.burnTicks--;
+
+
+        }
+    }
+
+    @Override
+    public void update() {
+        if (!worldObj.isRemote) {
+            if (this.burned && this.realSpeed == speed)
+                this.realSpeed += speed / 10;
+
+            funnelAction(realSpeed);
+        }
+        this.burnAction();
     }
 
 
