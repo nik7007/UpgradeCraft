153e006cded76e0c3c24738c36c02694dbb16fa8
Nik <niky006@gmail.com>
2016-12-28 21:48:00 +0100

    Compacting and add feature to vanilla interface for fluid handler

---
+++ src/main/java/com/nik7/upgcraft/fluids/tank/IUpgCFluidTank.java
@@ -0,0 +1,14 @@
+package com.nik7.upgcraft.fluids.tank;
+
+
+import com.nik7.upgcraft.nbt.INBTProvider;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+public interface IUpgCFluidTank extends IFluidTank, IFluidHandler, INBTProvider<IUpgCFluidTank> {
+
+    boolean canFill();
+
+    boolean canDrain();
+
+}
