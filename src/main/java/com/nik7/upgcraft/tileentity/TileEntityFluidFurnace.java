3437a52ea3798b600068254d103ad52ecfec8e4e
Nik <niky006@gmail.com>
2017-01-03 12:36:49 +0100

    Now the furnace smelts!

--- src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidFurnace.java
+++ src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidFurnace.java
@@ -3,8 +3,10 @@ package com.nik7.upgcraft.tileentity;
 
 import com.nik7.upgcraft.fluids.EnumCapacity;
 import com.nik7.upgcraft.fluids.tank.UpgCFluidTankWrapper;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.inventory.ItemStackHelper;
@@ -13,11 +15,18 @@ import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.IInteractionObject;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.SidedInvWrapper;
@@ -25,14 +34,25 @@ import net.minecraftforge.items.wrapper.SidedInvWrapper;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class TileEntityFluidFurnace extends TileEntityFluidHandler implements ISidedInventory, IInteractionObject {
+public class TileEntityFluidFurnace extends TileEntityFluidHandler implements ITickable, ISidedInventory, IInteractionObject {
 
-    private static final int[] SLOTS_TOP_SIDE = new int[]{0};
-    private static final int[] SLOTS_BOTTOM = new int[]{1};
+    private static final int INPUT = 0;
+    private static final int OUTPUT = 1;
 
+    private static final int[] SLOTS_TOP_SIDE = new int[]{INPUT};
+    private static final int[] SLOTS_BOTTOM = new int[]{OUTPUT};
+    private static final int LAVA_BURNING_TIME = getFluidBurningTime(new FluidStack(FluidRegistry.LAVA, 1));
+    private static final int LAVA_TEMPERATURE = FluidRegistry.LAVA.getTemperature();
     protected final NonNullList<ItemStack> inventory; // input : 0 - output : 1
     private IItemHandler inputInventory;
     private IItemHandler outputInventory;
+    private Fluid currentFluid = null;
+    private boolean isCurrentFluidValid = false;
+    private int fuelMaxLife = -1;
+    private int tickToSmelt = -1;
+
+    private int progress = 0;
+    private int fuelLife = 0;
 
     private UpgCFluidTankWrapper inputTank;
     private UpgCFluidTankWrapper outputTank;
@@ -48,10 +68,32 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
         this.outputInventory = new SidedInvWrapper(this, EnumFacing.DOWN);
     }
 
+    private static int getFluidBurningTime(FluidStack fluidStack) {
+        int burningTime = 0;
+
+        if (fluidStack != null && fluidStack.getFluid() != null && fluidStack.amount > 0) {
+            ItemStack bucket = new ItemStack(Items.BUCKET);
+            IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(bucket);
+
+            if (fluidHandler != null) {
+                if (fluidHandler.fill(new FluidStack(fluidStack, Fluid.BUCKET_VOLUME), true) == Fluid.BUCKET_VOLUME) {
+                    bucket = fluidHandler.getContainer();
+
+                    burningTime = TileEntityFurnace.getItemBurnTime(bucket);
+                }
+            }
+        }
+
+        return burningTime;
+    }
+
     @Override
     public void readFromNBT(NBTTagCompound tag) {
         super.readFromNBT(tag);
         ItemStackHelper.loadAllItems(tag, this.inventory);
+
+        this.progress = tag.getInteger("progress");
+        this.fuelLife = tag.getInteger("fuelLife");
 
         if (tag.hasKey("CustomName", 8)) {
             this.customName = tag.getString("CustomName");
@@ -61,8 +103,12 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
     @Override
     @Nonnull
     public NBTTagCompound writeToNBT(NBTTagCompound tag) {
-        tag = super.writeToNBT(tag);
+        super.writeToNBT(tag);
         ItemStackHelper.saveAllItems(tag, this.inventory);
+
+        tag.setInteger("progress", this.progress);
+        tag.setInteger("fuelLife", this.fuelLife);
+
         if (this.hasCustomName()) {
             tag.setString("CustomName", this.customName);
         }
@@ -94,7 +140,9 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
 
     @Override
     public void syncTileEntity() {
-
+        markDirty();
+        IBlockState state = getWorld().getBlockState(getPos());
+        getWorld().notifyBlockUpdate(getPos(), state, state, 3);
     }
 
     @Override
@@ -168,7 +216,6 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
         return this.getWorld().getTileEntity(this.getPos()) == this && player.getDistanceSq((double) this.getPos().getX() + 0.5D, (double) this.getPos().getY() + 0.5D, (double) this.getPos().getZ() + 0.5D) <= 64.0D;
     }
 
-
     @Override
     public void openInventory(@Nonnull EntityPlayer player) {
 
@@ -187,6 +234,99 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
         else {
             ItemStack smeltResult = FurnaceRecipes.instance().getSmeltingResult(stack);
             return !smeltResult.isEmpty();
+
+        }
+    }
+
+    private void invalidateFluid() {
+        if (this.isCurrentFluidValid) {
+            this.currentFluid = null;
+            this.isCurrentFluidValid = false;
+            this.fuelMaxLife = -1;
+            this.fuelMaxLife = -1;
+        }
+    }
+
+    private boolean canOperate() {
+
+
+        FluidStack fluid;
+        if ((fluid = getFluid()) != null && fluid.amount > 0) {
+            if (fluid.getFluid() != this.currentFluid) {
+                this.currentFluid = fluid.getFluid();
+                this.isCurrentFluidValid = getFluidBurningTime(fluid) > 0;
+            }
+            if (this.isCurrentFluidValid) {
+
+                ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(this.inventory.get(INPUT));
+
+                if (!itemstack.isEmpty()) {
+                    ItemStack outputItem = this.inventory.get(OUTPUT);
+                    if (outputItem.isEmpty())
+                        return true;
+                    else if (outputItem.isItemEqual(itemstack)) {
+
+                        int result = outputItem.getCount() + itemstack.getCount();
+                        return result <= getInventoryStackLimit() && result <= outputItem.getMaxStackSize();
+
+                    }
+                }
+
+            } else {
+                this.fuelMaxLife = -1;
+                this.fuelMaxLife = -1;
+            }
+
+        } else
+            invalidateFluid();
+
+
+        return false;
+
+    }
+
+    private void checkFluidSpeck() {
+
+        if (this.fuelMaxLife < 0 || this.tickToSmelt < 0) {
+            FluidStack fluidStack = getFluid();
+            this.fuelMaxLife = (int) (((getFluidBurningTime(fluidStack)) / (float) (LAVA_BURNING_TIME)) * 20);
+            this.tickToSmelt = (int) ((fluidStack.getFluid().getTemperature(fluidStack) / (float) LAVA_TEMPERATURE) * 200);
+
+        }
+    }
+
+    private void operate() {
+        if (++this.fuelLife >= this.fuelMaxLife) {
+            this.fuelLife = 0;
+            this.drain(1, true);
+        }
+        if (++this.progress >= this.tickToSmelt) {
+            this.progress = 0;
+            ItemStack input = this.inventory.get(INPUT);
+            ItemStack output = this.inventory.get(OUTPUT);
+            ItemStack result = FurnaceRecipes.instance().getSmeltingResult(input);
+
+            if (output.isEmpty())
+                this.inventory.set(OUTPUT, result.copy());
+            else output.grow(result.getCount());
+
+            if (input.getCount() == 1)
+                this.inventory.set(INPUT, ItemStack.EMPTY);
+            else input.shrink(1);
+
+        }
+    }
+
+    @Override
+    public void update() {
+
+        if (!getWorld().isRemote) {
+            if (canOperate()) {
+                checkFluidSpeck();
+                operate();
+
+            } else if (this.progress != 0)
+                this.progress--;
 
         }
     }
@@ -261,5 +401,6 @@ public class TileEntityFluidFurnace extends TileEntityFluidHandler implements IS
 
         return super.getCapability(capability, facing);
     }
+
 
 }
