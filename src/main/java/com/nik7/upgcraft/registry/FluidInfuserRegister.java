0899815e9ff4a78ca80b63cb6a5086a1f7a149da
Nik <niky006@gmail.com>
2017-01-30 18:12:06 +0100

    Start design fluid infuser register

--- src/main/java/com/nik7/upgcraft/registry/FluidInfuserRegister.java
+++ src/main/java/com/nik7/upgcraft/registry/FluidInfuserRegister.java
@@ -1,5 +1,20 @@
 package com.nik7.upgcraft.registry;
 
 
+import com.nik7.upgcraft.registry.recipes.FluidInfuserRecipe;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class FluidInfuserRegister {
+
+    private final static FluidInfuserRegister INSTANCE = new FluidInfuserRegister();
+
+    private final Set<FluidInfuserRecipe> recipes = new HashSet<>();
+
+    private FluidInfuserRegister() {
+
+    }
+
+
 }
