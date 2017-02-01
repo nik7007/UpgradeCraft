59e4724b30f55a84003cdc388aa26666a0b7a038
Nik <niky006@gmail.com>
2017-02-01 18:24:58 +0100

    Continuing design fluid infuser register

--- src/main/java/com/nik7/upgcraft/registry/FluidInfuserRegister.java
+++ src/main/java/com/nik7/upgcraft/registry/FluidInfuserRegister.java
@@ -2,8 +2,12 @@ package com.nik7.upgcraft.registry;
 
 
 import com.nik7.upgcraft.registry.recipes.FluidInfuserRecipe;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class FluidInfuserRegister {
@@ -12,8 +16,40 @@ public class FluidInfuserRegister {
 
     private final Set<FluidInfuserRecipe> recipes = new HashSet<>();
 
+    private final Map<ItemOD, Map<ItemOD, Map<FluidStack, FluidInfuserRecipe>>> toMeltToRecipe = new HashMap<>();
+    private final Map<ItemOD, Map<ItemOD, Map<FluidStack, FluidInfuserRecipe>>> toInfuseToRecipe = new HashMap<>();
+
+    private final Map<FluidStack, Set<ItemOD>> fluidToMelt = new HashMap<>();
+    private final Map<FluidStack, Set<ItemOD>> fluidToInfuse = new HashMap<>();
+
+    private final Map<ItemOD, Set<FluidStack>> toMeltFluid = new HashMap<>();
+    private final Map<ItemOD, Set<FluidStack>> toInfuseFluid = new HashMap<>();
+
     private FluidInfuserRegister() {
 
+    }
+
+    private static void registerFluidInfuserRecipe(FluidInfuserRecipe recipe) {
+        if (!INSTANCE.recipes.contains(recipe)) {
+            INSTANCE.recipes.add(recipe);
+
+            ItemOD toMeltKey = INSTANCE.createKey(recipe.getToMelt());
+            ItemOD toInfuseKey = INSTANCE.createKey(recipe.getToInfuse());
+            FluidStack fluidStackKey = INSTANCE.createKey(recipe.getFluidStack());
+
+
+        }
+    }
+
+    private ItemOD createKey(ItemStack original) {
+        original.setCount(1);
+        return new ItemOD(original);
+    }
+
+    private FluidStack createKey(FluidStack fluidStack) {
+        fluidStack.amount = 1;
+
+        return fluidStack;
     }
 
 
