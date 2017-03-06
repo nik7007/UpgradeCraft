57efedd87e489d766e8f5449b31e740b8a6af9af
Nik <niky006@gmail.com>
2017-03-06 13:55:59 +0100

    Working on fluid infuser

--- src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidInfuser.java
+++ src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidInfuser.java
@@ -1,5 +1,97 @@
 package com.nik7.upgcraft.tileentity;
 
 
-public class TileEntityFluidInfuser {
+import com.nik7.upgcraft.fluids.EnumCapacity;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraft.world.IInteractionObject;
+
+public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler implements ITickable, IInteractionObject {
+
+    public static final int[] INPUT = {0, 1};
+    public static final int OUTPUT = 4;
+
+
+    public TileEntityFluidInfuser() {
+        super(EnumCapacity.MACHINE_CAPACITY, 5);
+    }
+
+
+    @Override
+    public void syncTileEntity() {
+        markDirty();
+        IBlockState state = getWorld().getBlockState(getPos());
+        getWorld().notifyBlockUpdate(getPos(), state, state, 3);
+    }
+
+    @Override
+    public int[] getSlotsForFace(EnumFacing side) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
+        return false;
+    }
+
+    @Override
+    public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 0;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {
+
+    }
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
+        return null;
+    }
+
+    @Override
+    public String getGuiID() {
+        return null;
+    }
 }
