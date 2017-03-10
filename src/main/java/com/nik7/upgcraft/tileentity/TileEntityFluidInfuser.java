1f9a77e687b23f2e2a0c148105ca69c74854b63e
nik7 <nik7007@users.noreply.github.com>
2017-03-10 01:37:25 +0100

    Working on fluid infuser

--- src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidInfuser.java
+++ src/main/java/com/nik7/upgcraft/tileentity/TileEntityFluidInfuser.java
@@ -2,24 +2,54 @@ package com.nik7.upgcraft.tileentity;
 
 
 import com.nik7.upgcraft.fluids.EnumCapacity;
+import com.nik7.upgcraft.fluids.IFluidIO;
+import com.nik7.upgcraft.registry.FluidInfuserRegister;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.world.IInteractionObject;
 
-public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler implements ITickable, IInteractionObject {
+import javax.annotation.Nonnull;
 
-    public static final int[] INPUT = {0, 1};
-    public static final int OUTPUT = 4;
+public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler implements ITickable, IInteractionObject, IFluidIO {
+
+    public static final int MELT = 0;
+    public static final int INFUSE = 1;
+    public static final int OUTPUT = 2;
+
+    private ItemStack resultWorking = ItemStack.EMPTY;
 
 
     public TileEntityFluidInfuser() {
-        super(EnumCapacity.MACHINE_CAPACITY, 5);
+        super(EnumCapacity.MACHINE_CAPACITY, 3);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        if (tag.hasKey("resultWorking")) {
+            this.resultWorking = new ItemStack(tag.getCompoundTag("resultWorking"));
+        }
+    }
+
+    @Override
+    @Nonnull
+    public NBTTagCompound writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+
+        if (this.resultWorking != ItemStack.EMPTY) {
+            NBTTagCompound tagCompound = new NBTTagCompound();
+            this.resultWorking.writeToNBT(tagCompound);
+            tag.setTag("resultWorking", tagCompound);
+
+        }
+
+        return tag;
     }
 
 
@@ -32,22 +62,23 @@ public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler i
 
     @Override
     public int[] getSlotsForFace(EnumFacing side) {
-        return new int[0];
+
+        return new int[]{MELT, INFUSE, OUTPUT};
     }
 
     @Override
     public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
-        return false;
+        return this.isItemValidForSlot(index, itemStackIn);
     }
 
     @Override
     public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
-        return false;
+        return index == OUTPUT;
     }
 
     @Override
     public int getInventoryStackLimit() {
-        return 0;
+        return 64;
     }
 
     @Override
@@ -62,6 +93,13 @@ public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler i
 
     @Override
     public boolean isItemValidForSlot(int index, ItemStack stack) {
+
+        if (index == MELT) {
+            return FluidInfuserRegister.isInputCorrect(this.getFluid(), stack, this.getStackInSlot(INFUSE));
+        } else if (index == INFUSE) {
+            return FluidInfuserRegister.isInputCorrect(this.getFluid(), this.getStackInSlot(MELT), stack);
+        }
+
         return false;
     }
 
@@ -93,5 +131,15 @@ public class TileEntityFluidInfuser extends TileEntityInventoryAndFluidHandler i
     @Override
     public String getGuiID() {
         return null;
+    }
+
+    @Override
+    public boolean canFill() {
+        return FluidInfuserRegister.isInputCorrect(this.getFluid(), this.getStackInSlot(MELT), this.getStackInSlot(INFUSE));
+    }
+
+    @Override
+    public boolean canDrain() {
+        return true;
     }
 }
