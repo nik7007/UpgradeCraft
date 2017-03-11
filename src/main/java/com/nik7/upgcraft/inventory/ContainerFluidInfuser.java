72a9d115b2b7fd86941cea1b519a77fc8c8a4e76
Nik <niky006@gmail.com>
2017-03-11 01:43:24 +0100

    Now slots are in the correct place

--- src/main/java/com/nik7/upgcraft/inventory/ContainerFluidInfuser.java
+++ src/main/java/com/nik7/upgcraft/inventory/ContainerFluidInfuser.java
@@ -6,9 +6,7 @@ import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.SlotFurnaceOutput;
 
-import static com.nik7.upgcraft.tileentity.TileEntityFluidInfuser.INFUSE;
-import static com.nik7.upgcraft.tileentity.TileEntityFluidInfuser.MELT;
-import static com.nik7.upgcraft.tileentity.TileEntityFluidInfuser.OUTPUT;
+import static com.nik7.upgcraft.tileentity.TileEntityFluidInfuser.*;
 
 
 public class ContainerFluidInfuser extends ContainerUpgC {
@@ -17,10 +15,10 @@ public class ContainerFluidInfuser extends ContainerUpgC {
     public ContainerFluidInfuser(InventoryPlayer playerInventory, TileEntityFluidInfuser inventory) {
         super(inventory);
 
-        this.addSlotToContainer(new Slot(inventory, MELT, 56, 17));
-        this.addSlotToContainer(new Slot(inventory, INFUSE, 76, 17));
+        this.addSlotToContainer(new Slot(inventory, MELT, 52, 20));
+        this.addSlotToContainer(new Slot(inventory, INFUSE, 82, 20));
 
-        this.addSlotToContainer(new SlotFurnaceOutput(playerInventory.player, inventory, OUTPUT, 116, 35));
+        this.addSlotToContainer(new SlotFurnaceOutput(playerInventory.player, inventory, OUTPUT, 142, 38));
 
         addPlayerSlots(playerInventory, 8, 84);
     }
