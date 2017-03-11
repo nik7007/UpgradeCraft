d6e5681d7f1bb57c441141f59d57775f31d3f5dd
Nik <niky006@gmail.com>
2017-03-11 01:32:07 +0100

    Working on fluid infuser block:  - Gui  - Drop items when broken  - Can be renamed

--- src/main/java/com/nik7/upgcraft/block/BlockFluidInfuser.java
+++ src/main/java/com/nik7/upgcraft/block/BlockFluidInfuser.java
@@ -1,10 +1,20 @@
 package com.nik7.upgcraft.block;
 
 
+import com.nik7.upgcraft.UpgradeCraft;
+import com.nik7.upgcraft.reference.GUIs;
 import com.nik7.upgcraft.tileentity.TileEntityFluidInfuser;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -14,6 +24,42 @@ public class BlockFluidInfuser extends BlockOrientable implements ITileEntityPro
 
     public BlockFluidInfuser() {
         super(Material.IRON, "fluidinfuser");
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+        if (!worldIn.isRemote) {
+            playerIn.openGui(UpgradeCraft.instance, GUIs.FLUID_INFUSER.ordinal(), worldIn, pos.getX(), pos.getY(), pos.getZ());
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+
+        if (stack.hasDisplayName()) {
+            TileEntity te = worldIn.getTileEntity(pos);
+            if (te instanceof TileEntityFluidInfuser) {
+                ((TileEntityFluidInfuser) te).setCustomInventoryName(stack.getDisplayName());
+            }
+        }
+
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        TileEntity tileentity = worldIn.getTileEntity(pos);
+
+        if (tileentity instanceof TileEntityFluidInfuser) {
+            InventoryHelper.dropInventoryItems(worldIn, pos, (TileEntityFluidInfuser) tileentity);
+            worldIn.updateComparatorOutputLevel(pos, this);
+        }
+
+        super.breakBlock(worldIn, pos, state);
     }
 
     @Nullable
