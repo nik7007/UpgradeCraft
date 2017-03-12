45ce00e9b26206d8ee83ce4ff2099b8c6299dfa8
Nik <niky006@gmail.com>
2017-03-13 00:14:35 +0100

    Use custom experience interface

--- src/main/java/com/nik7/upgcraft/block/BlockSlimyLog.java
+++ src/main/java/com/nik7/upgcraft/block/BlockSlimyLog.java
@@ -1,12 +1,14 @@
 package com.nik7.upgcraft.block;
 
 
+import com.nik7.upgcraft.registry.ICraftingExperience;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
-public class BlockSlimyLog extends BlockUpgC {
+public class BlockSlimyLog extends BlockUpgC implements ICraftingExperience {
 
     public BlockSlimyLog() {
         super(Material.WOOD, "slimylog");
@@ -22,5 +24,10 @@ public class BlockSlimyLog extends BlockUpgC {
     @Override
     public int getFireSpreadSpeed(IBlockAccess world, BlockPos pos, EnumFacing face) {
         return 15;
+    }
+
+    @Override
+    public float getCraftingExperience(ItemStack item) {
+        return 0.6f;
     }
 }
